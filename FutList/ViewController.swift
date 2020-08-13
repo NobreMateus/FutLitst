641f5e8b5e308d55e691a19d7ae53276fa49fac5
Mateus <mateusnobref@gmail.com>
2020-08-13 10:38:39 -0300

    Initial Commit

---
+++ FutList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FutList
+//
+//  Created by Mateus Nobre on 13/08/20.
+//  Copyright © 2020 Mateus Nobre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
